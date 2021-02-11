2bfa154a0e63daaa237d153a2a4434a861d5b7b3
Abasheva Dasha <Abashevadasha@gmail.com>
2021-02-11 15:10:02 +0400

    Home work:create Variable class with main method

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    public static void main(String[] args) {
+
+        byte cores = 4; 
+        short memory = 16;
+        int typeSystem = 64;
+        long soket = 1155;
+        float coreVoltage = 1.184f;
+        double frequenseMemory = 1199.8d;
+        char processorCore7 = 'i';
+        boolean computer = true;
+
+                 System.out.println("byte cores = "+ cores);
+                 System.out.println("short memory = "+ memory);
+                 System.out.println("int typeSystem = "+ typeSystem);
+                 System.out.println("long soket = "+ soket);
+                 System.out.println("float coreVoltage = "+ coreVoltage);
+                 System.out.println("double frequenseMemory = "+ frequenseMemory);
+                 System.out.println("char processorCore7 = "+ processorCore7);
+                 System.out.println("boolean computer = "+ computer);
+    }
+}
