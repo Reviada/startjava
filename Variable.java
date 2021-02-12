6229d5d2cda3224074d8aa90bb5b80bbc05df1a4
Abasheva Dasha <Abashevadasha@gmail.com>
2021-02-12 11:55:24 +0400

    Error correction

--- Variable.java
+++ Variable.java
@@ -6,17 +6,16 @@ public class Variable {
         int typeSystem = 64;
         long soket = 1155;
         float coreVoltage = 1.184f;
-        double frequenseMemory = 1199.8d;
-        char processorCore7 = 'i';
-        boolean computer = true;
-
-                 System.out.println("byte cores = "+ cores);
-                 System.out.println("short memory = "+ memory);
-                 System.out.println("int typeSystem = "+ typeSystem);
-                 System.out.println("long soket = "+ soket);
-                 System.out.println("float coreVoltage = "+ coreVoltage);
-                 System.out.println("double frequenseMemory = "+ frequenseMemory);
-                 System.out.println("char processorCore7 = "+ processorCore7);
-                 System.out.println("boolean computer = "+ computer);
+        double freq = 1199.8d;
+        char cpu = 'i';
+        boolean comp = true;
+        System.out.println("byte cores = " + cores);
+        System.out.println("short memory = " + memory);
+        System.out.println("int typeSystem = " + typeSystem);
+        System.out.println("long soket = " + soket);
+        System.out.println("float coreVoltage = " + coreVoltage);
+        System.out.println("double freq = " + freq);
+        System.out.println("char cpu = " + cpu);
+        System.out.println("boolean comp = " + comp);
     }
 }
