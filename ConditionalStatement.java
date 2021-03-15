8f8f62ddcda70b36164d1e243d7b5e6c637f8949
Abasheva Dasha <Abashevadasha@gmail.com>
2021-03-15 15:19:42 +0400

    Home work ConditionalStatement class(error correction)

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -1,26 +1,28 @@
-public class ConditionalStatement{
+public class ConditionalStatement {
     public static void main(String[] args) {
         int age = 23;
-        boolean  maleGender = false;
-        double height = 1.95;
-        char firstLetterName = 'L';
-
         if (age > 20) {
-            System.out.println ("You are  " + age + "  and your age is over 20 years");
-        }  if (maleGender) {
-            System.out.println ("Male gender");
+            System.out.println("You are  " + age + "  and your age is over 20 years");
+        }  
+        boolean maleGender = false;
+        if (maleGender) {
+            System.out.println("Male gender");
         } else if (!maleGender) {
-            System.out.println ("Famale gender");
-        } if (height < 1.80) {
-            System.out.println ("Your height  "+ height +  " it is less than 1.80");
+            System.out.println("Famale gender");
+        }
+        double height = 1.95;
+        if (height < 1.80) {
+            System.out.println("Your height  "+ height +  " it is less than 1.80");
         } else {
-            System.out.println ("Your height  "+ height +  " it is over 1.80");
-        } if (firstLetterName == 'M') {
-            System.out.println ("Your name starts with М");
+            System.out.println("Your height  "+ height +  " it is over 1.80");
+        }
+        char firstLetterName = 'L';
+        if (firstLetterName == 'M') {
+            System.out.println("Your name starts with М");
         } else if (firstLetterName == 'I') {
-            System.out.println ("Your name starts with I");
+            System.out.println("Your name starts with I");
         } else {
-            System.out.println ("Your name starts with a different letter, but not 'М' and but not 'I'");
+            System.out.println("Your name starts with a different letter, but not 'М' and but not 'I'");
         }
     }
 }
