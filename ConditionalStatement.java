70764fdca5532c69436d77ecaf9844caf00ac4f4
Abasheva Dasha <Abashevadasha@gmail.com>
2021-03-16 19:54:15 +0400

    Home work ConditionalStatement class(error correction2)

--- ConditionalStatement.java
+++ ConditionalStatement.java
@@ -3,19 +3,23 @@ public class ConditionalStatement {
         int age = 23;
         if (age > 20) {
             System.out.println("You are  " + age + "  and your age is over 20 years");
-        }  
+        } 
+
         boolean maleGender = false;
         if (maleGender) {
             System.out.println("Male gender");
         } else if (!maleGender) {
             System.out.println("Famale gender");
         }
+
         double height = 1.95;
         if (height < 1.80) {
             System.out.println("Your height  "+ height +  " it is less than 1.80");
         } else {
             System.out.println("Your height  "+ height +  " it is over 1.80");
         }
+
+        
         char firstLetterName = 'L';
         if (firstLetterName == 'M') {
             System.out.println("Your name starts with М");
