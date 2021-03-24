419d42bb9c3870d4cff76cf3662ca4dbace572e7
Abasheva Dasha <Abashevadasha@gmail.com>
2021-03-24 18:21:48 +0400

    Home work Cycle class error correction

--- Cycle.java
+++ Cycle.java
@@ -3,24 +3,23 @@ public class Cycle {
         System.out.println("1.All numbers from [0, 20] :");
         for (int i = 0; i <= 20; i++) {
             System.out.println(i);
-       }
+        }
+
         System.out.println("2.All numbers from [6, -6] (with iteration step 2) :");
-        int i = 2;
         int result = 6;
         while (result >= -6) {
             System.out.println(result);
-            result = result - i;
-       } 
-        System.out.println("3.The sum of odd numbers from [10, 20] :");
+            result = result - 2;
+        }
+
         int i1 = 10;
         int sum = 0;
         do {
            if (i1 % 2 != 0) {
                sum += i1;
-             System.out.println(i1);
-            }   
+           }   
             i1++;
         } while (i1 <= 20);
-             System.out.println("Sum of odd numbers = " + sum);
+        System.out.println("3.Sum of odd numbers = " + sum);
     }
 }
