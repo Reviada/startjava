239e877711ad7a17350526a2ae00e0e588c8d912
Abasheva Dasha <Abashevadasha@gmail.com>
2021-05-06 23:15:30 +0400

    MyFirstGame error correction 1

--- MyFirstGame.java
+++ MyFirstGame.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 
 public class MyFirstGame {
     public static void main(String[] args) {
-        int i = (int) (Math.random () * 100 + 1);
-        int a;
+        int guess = (int) (Math.random() * 100 + 1);
+        int number;
+           System.out.println("Guess the number from 0 to 100");
+           Scanner scanner = new Scanner(System.in);
+            
         do {
+            number = scanner.nextInt();
 
-            System.out.println("Guess the number from 0 to 100");
-            Scanner scanner = new Scanner (System.in);
-            a = scanner.nextInt();
-        
-        if (a > i) {
+        if (number > guess) {
             System.out.println("This number is greater than what the computer guessed");
+        }
 
-        } else if (a < i) {
+        else if (number < guess) {
             System.out.println("This number is less than what the computer less");
         }
 
-        }while (a != i);
-    System.out.println("Congratulations, the number " + a + " is right!");
+        } while (number != guess); 
+    System.out.println("Congratulations, the number " + number + " is right!");
   }
-}
+ }
